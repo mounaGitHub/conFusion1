96cf53e25b8ca730bb7b793462171f3caf448fb7
mouna.yangui <mouna.yangui@gmail.com>
2020-12-16 13:25:53 +0100

    Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,18 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +33,9 @@ import { MatButtonModule } from '@angular/material/button';
     MatButtonModule,
     MatCardModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
