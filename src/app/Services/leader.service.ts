89bb67fc2024fb9a33a3cb362cbf33322e5397a5
mouna.yangui <mouna.yangui@gmail.com>
2020-12-21 15:24:13 +0100

    RxJS Part 2

--- src/app/Services/leader.service.ts
+++ src/app/Services/leader.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,23 +12,27 @@ export class LeaderService {
   constructor() { }
 
 
-  getLEADERS():  Promise<Leader[]> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(LEADERS), 2000);
-    });
+  getLEADERS():  Observable<Leader[]> {
+    // return new Promise(resolve=> {
+    //   // Simulate server latency with 2 second delay
+    //     setTimeout(() => resolve(LEADERS), 2000);
+    // });
+
+    return of(LEADERS).pipe(delay(2000));
   }
-  getLeader(id: string):  Promise<Leader> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
-    });
+  getLeader(id: string):  Observable<Leader> {
+    // return new Promise(resolve=> {
+    //   // Simulate server latency with 2 second delay
+    //     setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
+    // });
+    return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
   }
 
-  getFeaturedLeader(): Promise<Leader> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000);
-    });
+  getFeaturedLeader(): Observable<Leader> {
+    // return new Promise(resolve=> {
+    //   // Simulate server latency with 2 second delay
+    //     setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000);
+    // });
+    return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
   }
 }
